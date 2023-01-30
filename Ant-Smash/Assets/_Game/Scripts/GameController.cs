252dc1f99ce4a9da3b122c9e17f933ba9e184a28
Emerson-02 <69188698+Emerson-02@users.noreply.github.com>
2023-01-30 11:24:50 -0300

    Exibindo pontuação

--- Ant-Smash/Assets/_Game/Scripts/GameController.cs
+++ Ant-Smash/Assets/_Game/Scripts/GameController.cs
@@ -6,11 +6,12 @@ public class GameController : MonoBehaviour
 {
 
     public AudioClip[] audioEnemies;
+    [HideInInspector] public int totalScore;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalScore = 0;
     }
 
     // Update is called once per frame
