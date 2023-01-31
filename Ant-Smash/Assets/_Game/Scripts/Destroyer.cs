af73410bcc30f5fb961bb744ce17d3255c5b4776
Emerson-02 <69188698+Emerson-02@users.noreply.github.com>
2023-01-31 19:34:31 -0300

    Reduzindo vidas do Player

--- Ant-Smash/Assets/_Game/Scripts/Destroyer.cs
+++ Ant-Smash/Assets/_Game/Scripts/Destroyer.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
+    private GameController gameController;
+    private UIController uiController;
+
+    private void Start() 
+    {
+        gameController = FindObjectOfType<GameController>();
+        uiController = FindObjectOfType<UIController>();
+    }
     private void OnTriggerEnter2D(Collider2D target) 
     {
         if(target.gameObject.CompareTag("Enemy"))
         {
+            gameController.enemyCount++;
+
+            if(gameController.enemyCount < 5)
+            {
+                uiController.imageLifes[gameController.enemyCount - 1].gameObject.SetActive(false);
+            }
+            else 
+            {
+                uiController.imageLifes[gameController.enemyCount - 1].gameObject.SetActive(false);
+                Debug.Log("Game Over");
+            }
+            
             Destroy(target.gameObject);
         }
+
+        
     }
 }
--- Ant-Smash/Assets/_Game/Scripts/GameController.cs
+++ Ant-Smash/Assets/_Game/Scripts/GameController.cs
@@ -7,11 +7,14 @@ public class GameController : MonoBehaviour
 
     public AudioClip[] audioEnemies;
     [HideInInspector] public int totalScore;
+    [HideInInspector] public int enemyCount;
 
     // Start is called before the first frame update
     void Start()
     {
         totalScore = 0;
+        enemyCount = 0;
+        
     }
 
     // Update is called once per frame
